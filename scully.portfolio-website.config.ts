3f95f0c9a75b456f16d8af7906bd2a89d36254cb
Amine AML <amin_aml@live.fr>
2021-08-23 21:31:18 +0100

    chore: update a tag attribute for links and Scully

--- scully.portfolio-website.config.ts
+++ scully.portfolio-website.config.ts
@@ -1,8 +1,22 @@
 import { ScullyConfig } from '@scullyio/scully';
+import { getFlashPreventionPlugin } from '@scullyio/scully-plugin-flash-prevention';
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "portfolio-website",
   outDir: './dist/static',
   routes: {
-  }
+    '/': {
+      type: 'json',
+      // slug: {
+      //   url: 'https://europe-west6-project-owlly.cloudfunctions.net/owlly/',
+      //   property: 'data.slug',
+      // },
+      postRenderers: [getFlashPreventionPlugin()],
+    },
+  },
+  puppeteerLaunchOptions: {
+    executablePath: 'chromium'
+  },
+  // defaultPostRenderers : [getFlashPreventionPlugin()],
 };
