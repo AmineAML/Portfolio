4cc454bd418afb66d8828daf7e537d8be0852ed2
AmineAml <amine.amellouk@outlook.com>
2022-04-13 03:05:23 +0000

    chore: upgrade package to compatible angular v12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,7 @@ const app_routes: Routes = [
     path: WebsiteLanguage.English, children: [
       {
         path: '',
-        loadChildren: () => import('src/app/layouts/welcome/welcome.languages.module').then(m => m.WelcomeEnModule),
+        loadChildren: () => import('./layouts/welcome/welcome.languages.module').then(m => m.WelcomeEnModule),
         data: {
           metadata: {
             title: 'Amine Amellouk | Software Developer',
@@ -49,148 +49,148 @@ const app_routes: Routes = [
           animation: 'welcome'
         }
       },
-      // {
-      //   path: 'resume',
-      //   loadChildren: () => import('./layouts/resume/resume.languages.module').then(m => m.ResumeEnModule),
-      //   data: {
-      //     metadata: {
-      //       title: 'Resume | Amine Amellouk',
-      //       description: 'My resume'
-      //     },
-      //     animation: 'resume'
-      //   }
-      // },
-      // {
-      //   path: 'pdf',
-      //   loadChildren: () => import('./layouts/pdf/pdf.languages.module').then(m => m.PdfEnModule),
-      //   data: {
-      //     metadata: {
-      //       title: "Thesis",
-      //       description: "Thesis list"
-      //     },
-      //     animation: 'pdf'
-      //   }
-      // },
-      // {
-      //   path: 'moodboard',
-      //   loadChildren: () => import('./layouts/moodboard/moodboard.languages.module').then(m => m.MoodboardEnModule),
-      //   data: {
-      //     metadata: {
-      //       title: 'Moodboard | Amine Amellouk',
-      //       description: 'Other portfolio projects.'
-      //     },
-      //     animation: 'moodboard'
-      //   }
-      // },
-      // {
-      //   path: 'unavailable',
-      //   loadChildren: () => import('./layouts/not-available/not-available.languages.module').then(m => m.NotAvailableEnModule),
-      //   data: {
-      //     metadata: {
-      //       title: 'Unavailable',
-      //       description: 'An error occured',
-      //       robots: 'noindex, nofollow'
-      //     }
-      //   }
-      // },
-      // {
-      //   path: '404',
-      //   loadChildren: () => import('./layouts/error/error.languages.module').then(m => m.ErrorEnModule),
-      //   data: {
-      //     metadata: {
-      //       title: '404',
-      //       description: 'Page not found',
-      //       robots: 'noindex, nofollow'
-      //     }
-      //   }
-      // },
-      // //This route doesn't work with npm scully:serve, it does work the http-server
-      // { path: '**', redirectTo: '404' }
+      {
+        path: 'resume',
+        loadChildren: () => import('./layouts/resume/resume.languages.module').then(m => m.ResumeEnModule),
+        data: {
+          metadata: {
+            title: 'Resume | Amine Amellouk',
+            description: 'My resume'
+          },
+          animation: 'resume'
+        }
+      },
+      {
+        path: 'pdf',
+        loadChildren: () => import('./layouts/pdf/pdf.languages.module').then(m => m.PdfEnModule),
+        data: {
+          metadata: {
+            title: "Thesis",
+            description: "Thesis list"
+          },
+          animation: 'pdf'
+        }
+      },
+      {
+        path: 'moodboard',
+        loadChildren: () => import('./layouts/moodboard/moodboard.languages.module').then(m => m.MoodboardEnModule),
+        data: {
+          metadata: {
+            title: 'Moodboard | Amine Amellouk',
+            description: 'Other portfolio projects.'
+          },
+          animation: 'moodboard'
+        }
+      },
+      {
+        path: 'unavailable',
+        loadChildren: () => import('./layouts/not-available/not-available.languages.module').then(m => m.NotAvailableEnModule),
+        data: {
+          metadata: {
+            title: 'Unavailable',
+            description: 'An error occured',
+            robots: 'noindex, nofollow'
+          }
+        }
+      },
+      {
+        path: '404',
+        loadChildren: () => import('./layouts/error/error.languages.module').then(m => m.ErrorEnModule),
+        data: {
+          metadata: {
+            title: '404',
+            description: 'Page not found',
+            robots: 'noindex, nofollow'
+          }
+        }
+      },
+      //This route doesn't work with npm scully:serve, it does work the http-server
+      { path: '**', redirectTo: '404' }
     ]
   },
-  // {
-  //   path: WebsiteLanguage.French, children: [
-  //     {
-  //       path: '',
-  //       loadChildren: () => import('./layouts/welcome/welcome.languages.module').then(m => m.WelcomeFrModule),
-  //       data: {
-  //         metadata: {
-  //           title: 'Amine Amellouk | Développeur Full-Stack',
-  //           description: 'Un développeur de logiciels avec une formation en économie qui développe des applications API, SSG, SPA et Jamstack.'
-  //         },
-  //         animation: 'welcome'
-  //       }
-  //     },
-  //     {
-  //       path: 'resume',
-  //       loadChildren: () => import('./layouts/resume/resume.languages.module').then(m => m.ResumeFrModule),
-  //       data: {
-  //         metadata: {
-  //           title: 'CV | Amine Amellouk',
-  //           description: 'Mon CV'
-  //         },
-  //         animation: 'resume'
-  //       }
-  //     },
-  //     {
-  //       path: 'pdf',
-  //       loadChildren: () => import('./layouts/pdf/pdf.languages.module').then(m => m.PdfFrModule),
-  //       data: {
-  //         metadata: {
-  //           title: "PFE",
-  //           description: "Liste des projet de fin d'études"
-  //         },
-  //         animation: 'pdf'
-  //       }
-  //     },
-  //     {
-  //       path: 'moodboard',
-  //       loadChildren: () => import('./layouts/moodboard/moodboard.languages.module').then(m => m.MoodboardFrModule),
-  //       data: {
-  //         metadata: {
-  //           title: 'Moodboard | Amine Amellouk',
-  //           description: 'D\'autres projets du portfolio.'
-  //         },
-  //         animation: 'moodboard'
-  //       }
-  //     },
-  //     {
-  //       path: 'unavailable',
-  //       loadChildren: () => import('./layouts/not-available/not-available.languages.module').then(m => m.NotAvailableEnModule),
-  //       data: {
-  //         metadata: {
-  //           title: 'Indisponible',
-  //           description: 'Un erreur est sourvenu',
-  //           robots: 'noindex, nofollow'
-  //         }
-  //       }
-  //     },
-  //     {
-  //       path: '404',
-  //       loadChildren: () => import('./layouts/error/error.languages.module').then(m => m.ErrorFrModule),
-  //       data: {
-  //         metadata: {
-  //           title: '404',
-  //           description: 'Page non trouvée',
-  //           robots: 'noindex, nofollow'
-  //         }
-  //       }
-  //     },
-  //     //This route doesn't work with npm scully:serve, it does work the http-server
-  //     { path: '**', redirectTo: '404' }
-  //   ]
-  // },
-  // {
-  //   path: 'hi',
-  //   loadChildren: () => import('./layouts/hi/hi.module').then(m => m.HiModule),
-  //   data: {
-  //     metadata: {
-  //       //title with many languages
-  //       title: 'Hi | Amine Amellouk',
-  //       description: 'Hello World'
-  //     }
-  //   }
-  // },
+  {
+    path: WebsiteLanguage.French, children: [
+      {
+        path: '',
+        loadChildren: () => import('./layouts/welcome/welcome.languages.module').then(m => m.WelcomeFrModule),
+        data: {
+          metadata: {
+            title: 'Amine Amellouk | Développeur Full-Stack',
+            description: 'Un développeur de logiciels avec une formation en économie qui développe des applications API, SSG, SPA et Jamstack.'
+          },
+          animation: 'welcome'
+        }
+      },
+      {
+        path: 'resume',
+        loadChildren: () => import('./layouts/resume/resume.languages.module').then(m => m.ResumeFrModule),
+        data: {
+          metadata: {
+            title: 'CV | Amine Amellouk',
+            description: 'Mon CV'
+          },
+          animation: 'resume'
+        }
+      },
+      {
+        path: 'pdf',
+        loadChildren: () => import('./layouts/pdf/pdf.languages.module').then(m => m.PdfFrModule),
+        data: {
+          metadata: {
+            title: "PFE",
+            description: "Liste des projet de fin d'études"
+          },
+          animation: 'pdf'
+        }
+      },
+      {
+        path: 'moodboard',
+        loadChildren: () => import('./layouts/moodboard/moodboard.languages.module').then(m => m.MoodboardFrModule),
+        data: {
+          metadata: {
+            title: 'Moodboard | Amine Amellouk',
+            description: 'D\'autres projets du portfolio.'
+          },
+          animation: 'moodboard'
+        }
+      },
+      {
+        path: 'unavailable',
+        loadChildren: () => import('./layouts/not-available/not-available.languages.module').then(m => m.NotAvailableEnModule),
+        data: {
+          metadata: {
+            title: 'Indisponible',
+            description: 'Un erreur est sourvenu',
+            robots: 'noindex, nofollow'
+          }
+        }
+      },
+      {
+        path: '404',
+        loadChildren: () => import('./layouts/error/error.languages.module').then(m => m.ErrorFrModule),
+        data: {
+          metadata: {
+            title: '404',
+            description: 'Page non trouvée',
+            robots: 'noindex, nofollow'
+          }
+        }
+      },
+      //This route doesn't work with npm scully:serve, it does work the http-server
+      { path: '**', redirectTo: '404' }
+    ]
+  },
+  {
+    path: 'hi',
+    loadChildren: () => import('./layouts/hi/hi.module').then(m => m.HiModule),
+    data: {
+      metadata: {
+        //title with many languages
+        title: 'Hi | Amine Amellouk',
+        description: 'Hello World'
+      }
+    }
+  },
 
   //Tried different ways to implement a redirect to 404, still it doesn't work with Scully
   /*//Use the local storage to detect which language the user's using and remove this " from 1st character and this " from last character
